064c34580c80474b256785c51f9c65a5a3acdd47
Dayoung98 <58020493+Dayoung98@users.noreply.github.com>
2021-04-05 16:58:47 +0900

    2141_우체국

---
+++ 백준_2141_우체국/src/Main.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N;
+	static long result=0;
+	static Node node[];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N= Integer.parseInt(st.nextToken());
+		node = new Node[N];
+		
+		for(int i=0;i<N;i++) {
+			st = new StringTokenizer(br.readLine());
+			long X = Long.parseLong(st.nextToken());
+			long A = Long.parseLong(st.nextToken());
+			node[i] = new Node(X,A);
+			result +=A;
+		}
+		
+		Arrays.sort(node); //입력값이 순서대로 들어온다는 보장 X
+		
+		long sum = 0;
+		
+		for(Node n : node) {//하나씩 인구 수 계산하여 중간 값과 가장 근접한 마을 찾아서 출력하기
+			sum += n.A;
+			if(sum >=(result+1)/2) {
+				System.out.println(String.valueOf(n.X));
+				break;
+			}
+		}
+
+		
+	}
+	
+	static class Node implements Comparable<Node>{
+		long X; //거리
+		long A; //사람
+		public Node(long x, long a) {
+			super();
+			X = x;
+			A = a;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			if(this.X == o.X) {
+				//서로 거리가 같으면
+				return (int) (this.A-o.A); //사람인구 순서 대로
+			}
+			return (int) (this.X-o.X); //거리 순서대로
+		}
+	}
+}
